cdddeed079a0ee89bdd0f403a654f684e6a37f9b
Nicolás García <nicolasg.ngl@gmail.com>
2026-05-15 23:35:43 -0500

    Add Estudiantes class and create registration

---
+++ Instructions.md
@@ -0,0 +1,9 @@
+[] Add studients
+[]Show the list of students
+[]Filter by the ID
+[] Stop exec
+[] Control structures (is, switch, while, for)
+[] variables
+[] Array or list for storage the studients
+[] Reusable Code
+[]try/catch
--- Program.java
+++ Program.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class Program {
+
+    static Estudiante[] estudiantes = new Estudiante[5];
+    static int contador = 0;
+
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+
+        Program.RegistrarEstudiante();
+
+    }
+
+    public static void MostrarEstudiantes(){
+
+    }
+
+    public static void RegistrarEstudiante() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese nombre de estudiante");
+        String nombre = sc.nextLine();
+
+        System.out.println("Ingrese programa del estudiante");
+        String carrera = sc.nextLine();
+
+        String id = "S" + (contador + 1);
+
+        estudiantes[contador] = new Estudiante(id, nombre, carrera);
+
+        System.out.println(estudiantes[contador]);
+        contador++;
+        sc.close();
+    }
+
+}
+
+class Estudiante {
+    String ID;
+    String Nombre;
+    String Carrera;
+
+    Estudiante(String id, String nombre, String carrera) {
+        this.ID = id;
+        this.Nombre = nombre;
+        this.Carrera = carrera;
+    }
+
+    @Override
+    public String toString() {
+        return ID + " - " + Nombre + " - " + Carrera;
     }
 }
